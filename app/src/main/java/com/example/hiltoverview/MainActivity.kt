f5c277c4e2877f629e2e2492b03d3c40c55cde80
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-17 01:48:08 +0530

    Field injection on Activity class

--- app/src/main/java/com/example/hiltoverview/MainActivity.kt
+++ app/src/main/java/com/example/hiltoverview/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.hiltoverview
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.hiltoverview.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+//@AndroidEntryPoint -- Annotation for Hilt Activity class
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    @Inject
+    lateinit var userRepository: UserRepository
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
+        userRepository.saveUser("prerna@gmail.com","1111")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/com/example/hiltoverview/UserApplication.kt
+++ app/src/main/java/com/example/hiltoverview/UserApplication.kt
@@ -4,17 +4,13 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
-//@HiltAndroidApp -- Annotation for Hilt Android Application
+//@HiltAndroidApp -- Annotation for Hilt Android Application class
 
 @HiltAndroidApp
 class UserApplication :Application() {
 
-      @Inject
-      lateinit var userRepository: UserRepository
-
     override fun onCreate() {
         super.onCreate()
 
-        userRepository.saveUser("prerna@gmail.com","1111")
     }
 }
